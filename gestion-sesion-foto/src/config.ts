ada4ed23da9459a6e4d3fbff9814a44e0ace1188
zastv <132970150+zastv@users.noreply.github.com>
2025-08-07 05:06:18 +0000

    aguita con pan

--- gestion-sesion-foto/src/config.ts
+++ gestion-sesion-foto/src/config.ts
@@ -1,2 +1,4 @@
 // Configuración de la aplicación
-export const API_BASE_URL = "https://orange-space-giggle-vx5v6qg5jgwcwxj5-4000.app.github.dev";
+export const API_BASE_URL = import.meta.env.PROD 
+  ? "https://orange-space-giggle-vx5v6qg5jgwcwxj5-4000.app.github.dev" 
+  : "http://localhost:4000";
